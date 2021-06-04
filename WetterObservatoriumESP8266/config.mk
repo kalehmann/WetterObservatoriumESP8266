25629231044bff55a13440124d049229885ee4f4
Karsten Lehmann <mail@kalehmann.de>
2021-06-04 16:36:03 +0200

    feat: Include all libraries in build process

--- WetterObservatoriumESP8266/config.mk
+++ WetterObservatoriumESP8266/config.mk
@@ -1,6 +1,10 @@
 THIS_DIR := $(realpath $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
 ROOT := $(THIS_DIR)/..
 LIBS =  \
-  $(ROOT)/vendor/esp8266/libraries
+  $(ROOT)/vendor/ADS1115_WE \
+  $(ROOT)/vendor/BME280 \
+  $(ROOT)/vendor/esp8266/libraries \
+  $(ROOT)/vendor/esp8266/libraries/Wire \
+  $(ROOT)/vendor/SimpleDHT
 
 UPLOAD_SPEED = 115200
