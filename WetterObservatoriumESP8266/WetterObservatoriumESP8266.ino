50e8ce7cbdafd2f41ba59fb5eb3b3b129c598a1f
Karsten Lehmann <mail@kalehmann.de>
2022-03-12 00:45:37 +0100

    fix: Tabs to spaces

--- WetterObservatoriumESP8266/WetterObservatoriumESP8266.ino
+++ WetterObservatoriumESP8266/WetterObservatoriumESP8266.ino
@@ -51,193 +51,193 @@ _Noreturn void fail(void);
 
 void setup(void)
 {
-	unsigned int bmp_retries = 5;
+        unsigned int bmp_retries = 5;
 
 #ifdef WITH_SERIAL_OUTPUT
-	Serial.begin(9600);
+        Serial.begin(9600);
 #endif
-	DEBUG_SERIAL("\nConnecting to WiFi with SSID \"");
-	DEBUG_SERIAL(WIFI_SSID);
-	DEBUG_SERIAL("\" ");
-	// Connect to WiFi
-	WiFi.begin(WIFI_SSID, WIFI_PASS);
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		DEBUG_SERIAL("*");
-	}
-	DEBUG_SERIAL(" - Success\n");
-	DEBUG_SERIAL("IP address : ");
-	DEBUG_SERIAL(WiFi.localIP());
-	// Automatically reconnect to WiFi on connection loss
+        DEBUG_SERIAL("\nConnecting to WiFi with SSID \"");
+        DEBUG_SERIAL(WIFI_SSID);
+        DEBUG_SERIAL("\" ");
+        // Connect to WiFi
+        WiFi.begin(WIFI_SSID, WIFI_PASS);
+        while (WiFi.status() != WL_CONNECTED) {
+                delay(500);
+                DEBUG_SERIAL("*");
+        }
+        DEBUG_SERIAL(" - Success\n");
+        DEBUG_SERIAL("IP address : ");
+        DEBUG_SERIAL(WiFi.localIP());
+        // Automatically reconnect to WiFi on connection loss
         WiFi.setAutoReconnect(true);
         WiFi.persistent(true);
 
-	Wire.begin();
+        Wire.begin();
 
-	DEBUG_SERIAL("\nConnecting to BMP280 pressure sensor ");
-	while(!bmp.begin()) {
-		DEBUG_SERIAL("*");
-		delay(1000);
-		if (0 == bmp_retries--) {
-		        DEBUG_SERIAL("No BMP280 connected\n");
-			has_bmp = false;
-			break;
-		}
-	}
-	DEBUG_SERIAL("\nConnecting to ADS1115 ADC ");
-	if(!adc.init()) {
-	        DEBUG_SERIAL("No ADS1115 connected!\n");
-		has_adc = false;
-	}
-	DEBUG_SERIAL("\n");
-	adc.setVoltageRange_mV(ADS1115_RANGE_4096);
-	timeClient.begin();
+        DEBUG_SERIAL("\nConnecting to BMP280 pressure sensor ");
+        while(!bmp.begin()) {
+                DEBUG_SERIAL("*");
+                delay(1000);
+                if (0 == bmp_retries--) {
+                        DEBUG_SERIAL("No BMP280 connected\n");
+                        has_bmp = false;
+                        break;
+                }
+        }
+        DEBUG_SERIAL("\nConnecting to ADS1115 ADC ");
+        if(!adc.init()) {
+                DEBUG_SERIAL("No ADS1115 connected!\n");
+                has_adc = false;
+        }
+        DEBUG_SERIAL("\n");
+        adc.setVoltageRange_mV(ADS1115_RANGE_4096);
+        timeClient.begin();
 }
 
 void loop(void)
 {
         unsigned long now = millis();
-	if (previousMeasurement != 0
-	    && now - previousMeasurement < MEASURE_INTERVAL * 1000) {
-	        return;
-	}
-	previousMeasurement = now;
-	timeClient.update();
-	sendData();
+        if (previousMeasurement != 0
+            && now - previousMeasurement < MEASURE_INTERVAL * 1000) {
+                return;
+        }
+        previousMeasurement = now;
+        timeClient.update();
+        sendData();
 }
 
 String dateHeader(void)
 {
         static String day_names[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         time_t rawtime;
-	struct tm *info;
-	rawtime = timeClient.getEpochTime();
-	info = gmtime(&rawtime);
-	String daysStr = info->tm_mday < 10
-	  ? "0" + String(info->tm_mday)
-	  : String(info->tm_mday);
-	String monthsStr = info->tm_mon < 9
-	  ? "0" + String(info->tm_mon + 1)
-	  : String(info->tm_mon + 1);
-	String yearsStr = String(info->tm_year + 1900);
-	String hoursStr = info->tm_hour < 10
-	  ? "0" + String(info->tm_hour)
-	  : String(info->tm_hour);
-	String minuteStr = info->tm_min < 10
-	  ? "0" + String(info->tm_min)
-	  : String(info->tm_min);
-	String secondStr = info->tm_sec < 10
-	  ? "0" + String(info->tm_sec)
-	  : String(info->tm_sec);
+        struct tm *info;
+        rawtime = timeClient.getEpochTime();
+        info = gmtime(&rawtime);
+        String daysStr = info->tm_mday < 10
+          ? "0" + String(info->tm_mday)
+          : String(info->tm_mday);
+        String monthsStr = info->tm_mon < 9
+          ? "0" + String(info->tm_mon + 1)
+          : String(info->tm_mon + 1);
+        String yearsStr = String(info->tm_year + 1900);
+        String hoursStr = info->tm_hour < 10
+          ? "0" + String(info->tm_hour)
+          : String(info->tm_hour);
+        String minuteStr = info->tm_min < 10
+          ? "0" + String(info->tm_min)
+          : String(info->tm_min);
+        String secondStr = info->tm_sec < 10
+          ? "0" + String(info->tm_sec)
+          : String(info->tm_sec);
 
-	return day_names[info->tm_wday] + ", " + daysStr + " " + monthsStr +
-	  " " + yearsStr + " " + hoursStr + ":" + minuteStr + ":" + secondStr
-	  + " GMT";
+        return day_names[info->tm_wday] + ", " + daysStr + " " + monthsStr +
+          " " + yearsStr + " " + hoursStr + ":" + minuteStr + ":" + secondStr
+          + " GMT";
 }
 
 String postBody(void)
 {
         bool has_dht{true};
         float bmp_temp(NAN), bmp_hum(NAN), bmp_pres(NAN), voltage(0);
-	byte dht_hum(0), dht_temp(0);
-	BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-	BME280::PresUnit presUnit(BME280::PresUnit_hPa);
-	int err = dht22.read(&dht_temp, &dht_hum, NULL);
-	long rssi(0);
-	String res = "{";
-	if (SimpleDHTErrSuccess != err) {
-		DEBUG_SERIAL("Read DHT22 failed, err=");
-		DEBUG_SERIAL(SimpleDHTErrCode(err));
-		DEBUG_SERIAL(",");
-		DEBUG_SERIAL(SimpleDHTErrDuration(err));
-		DEBUG_SERIAL("\n");
-		has_dht = false;
-	}
-	if (has_bmp) {
-	        bmp.read(bmp_pres, bmp_temp, bmp_hum, tempUnit, presUnit);
-	}
-	if (has_adc) {
-	        adc.setCompareChannels(ADS1115_COMP_0_GND);
-		adc.startSingleMeasurement();
-		while(adc.isBusy()) {}
-		voltage = adc.getResult_mV();
-	}
+        byte dht_hum(0), dht_temp(0);
+        BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+        BME280::PresUnit presUnit(BME280::PresUnit_hPa);
+        int err = dht22.read(&dht_temp, &dht_hum, NULL);
+        long rssi(0);
+        String res = "{";
+        if (SimpleDHTErrSuccess != err) {
+                DEBUG_SERIAL("Read DHT22 failed, err=");
+                DEBUG_SERIAL(SimpleDHTErrCode(err));
+                DEBUG_SERIAL(",");
+                DEBUG_SERIAL(SimpleDHTErrDuration(err));
+                DEBUG_SERIAL("\n");
+                has_dht = false;
+        }
+        if (has_bmp) {
+                bmp.read(bmp_pres, bmp_temp, bmp_hum, tempUnit, presUnit);
+        }
+        if (has_adc) {
+                adc.setCompareChannels(ADS1115_COMP_0_GND);
+                adc.startSingleMeasurement();
+                while(adc.isBusy()) {}
+                voltage = adc.getResult_mV();
+        }
 
-	if (has_dht) {
-	        DEBUG_SERIAL("\nDHT22     Temp: ");
-		DEBUG_SERIAL(dht_temp);
-		DEBUG_SERIAL(" C Humidity ");
-		DEBUG_SERIAL(dht_hum);
-		DEBUG_SERIAL(" %\n");
-		res = res + "\"humidity\": " + String(dht_hum);
-	}
-	if (has_bmp) {
-	        DEBUG_SERIAL("BMP280    Temp: ");
-		DEBUG_SERIAL(bmp_temp);
-		DEBUG_SERIAL(" C Pressure: ");
-		DEBUG_SERIAL(bmp_pres);
-		DEBUG_SERIAL(" hPa\n");
-		if (has_dht) {
-		        res = res + ",";
-		}
-		res = res + "\"temperature\": " + String(bmp_temp) +
-		        ", \"pressure\": " + String(bmp_pres);
-	}
-	if (has_adc) {
-	        DEBUG_SERIAL("Sun: ");
-		DEBUG_SERIAL(voltage / 3000 * 100);
-		DEBUG_SERIAL(" %\n");
-		if (has_dht || has_bmp) {
-		        res = res + ",";
-		}
-		res = res + "\"sun\": " + String(voltage / 3600 * 100);
-	}
-	rssi = WiFi.RSSI();
-	if (has_dht || has_bmp || has_adc) {
-		res = res + ",";
-	}
-	res = res + "\"rssi\": " + String(rssi);
+        if (has_dht) {
+                DEBUG_SERIAL("\nDHT22     Temp: ");
+                DEBUG_SERIAL(dht_temp);
+                DEBUG_SERIAL(" C Humidity ");
+                DEBUG_SERIAL(dht_hum);
+                DEBUG_SERIAL(" %\n");
+                res = res + "\"humidity\": " + String(dht_hum);
+        }
+        if (has_bmp) {
+                DEBUG_SERIAL("BMP280    Temp: ");
+                DEBUG_SERIAL(bmp_temp);
+                DEBUG_SERIAL(" C Pressure: ");
+                DEBUG_SERIAL(bmp_pres);
+                DEBUG_SERIAL(" hPa\n");
+                if (has_dht) {
+                        res = res + ",";
+                }
+                res = res + "\"temperature\": " + String(bmp_temp) +
+                        ", \"pressure\": " + String(bmp_pres);
+        }
+        if (has_adc) {
+                DEBUG_SERIAL("Sun: ");
+                DEBUG_SERIAL(voltage / 3000 * 100);
+                DEBUG_SERIAL(" %\n");
+                if (has_dht || has_bmp) {
+                        res = res + ",";
+                }
+                res = res + "\"sun\": " + String(voltage / 3600 * 100);
+        }
+        rssi = WiFi.RSSI();
+        if (has_dht || has_bmp || has_adc) {
+                res = res + ",";
+        }
+        res = res + "\"rssi\": " + String(rssi);
 
-	return res + "}";
+        return res + "}";
 }
 
 void sendData(void)
 {
         String path = String("/api/") + LOCATION;
-	String body = postBody();
-	String date = dateHeader();
-	String toSign = String("date: ") + date + "\n" + body;
-	String hmac = experimental::crypto::SHA256::hmac(toSign, API_KEY, strlen(API_KEY), 32);
+        String body = postBody();
+        String date = dateHeader();
+        String toSign = String("date: ") + date + "\n" + body;
+        String hmac = experimental::crypto::SHA256::hmac(toSign, API_KEY, strlen(API_KEY), 32);
 
-	if ((WiFi.status() == WL_CONNECTED)) {
+        if ((WiFi.status() == WL_CONNECTED)) {
                 http.begin(client, API_HOST, API_PORT, path, false);
-		http.addHeader("Content-Type", "application/json");
-		http.addHeader("Date", dateHeader());
-		http.addHeader("Authorization", "hmac username=\"test\", algorithm=\"sha256\", headers=\"date\", signature=\"" + String(hmac.c_str()) + "\"");
-		int httpCode = http.POST(body);
-		if (httpCode > 0) {
-		        if (httpCode == HTTP_CODE_OK) {
-			        DEBUG_SERIAL("Data successfully sended\n");
-		        } else {
-			        DEBUG_SERIAL("Server error: HTTP code " + String(httpCode));
-		        }
-		} else {
-		        DEBUG_SERIAL("HTTP POST failed: " + http.errorToString(httpCode));
-		}
-		http.end();
-	}
+                http.addHeader("Content-Type", "application/json");
+                http.addHeader("Date", dateHeader());
+                http.addHeader("Authorization", "hmac username=\"test\", algorithm=\"sha256\", headers=\"date\", signature=\"" + String(hmac.c_str()) + "\"");
+                int httpCode = http.POST(body);
+                if (httpCode > 0) {
+                        if (httpCode == HTTP_CODE_OK) {
+                                DEBUG_SERIAL("Data successfully sended\n");
+                        } else {
+                                DEBUG_SERIAL("Server error: HTTP code " + String(httpCode));
+                        }
+                } else {
+                        DEBUG_SERIAL("HTTP POST failed: " + http.errorToString(httpCode));
+                }
+                http.end();
+        }
 }
 
 _Noreturn void fail(void)
 {
-	// Perform some random blinknig of the builtin LED to indicate a
-	// failure.
-	pinMode(LED_BUILTIN, OUTPUT);
-	for (unsigned int i = 0; i < 16; i++) {
-	  // digitalWrite(LED_BUILTIN, HIGH);
-		delay(600);
-		digitalWrite(LED_BUILTIN, LOW);
-		delay(600);
-	}
-	for (;;) {}
+        // Perform some random blinknig of the builtin LED to indicate a
+        // failure.
+        pinMode(LED_BUILTIN, OUTPUT);
+        for (unsigned int i = 0; i < 16; i++) {
+          // digitalWrite(LED_BUILTIN, HIGH);
+                delay(600);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(600);
+        }
+        for (;;) {}
 }
